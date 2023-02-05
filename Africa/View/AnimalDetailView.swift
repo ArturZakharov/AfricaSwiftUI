4d0ca1f5b36fcf509bca9c8c6b5ee9f1b7c0d6f6
ZArthur88 <z.artur1988@gmail.com>
2023-02-05 21:16:34 +0200

    Add AnimalDetail view

---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,61 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by ArturZaharov on 02.02.2023.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    //MARK: - PROPERTIES
+    let animal: Animal
+    
+    //MARK: - BODY
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(alignment: .center, spacing: 20) {
+                //Hero Image
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                //Title
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                //Headline
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                //Gallery
+                HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    .padding(.horizontal)
+                CaruselGalleryView(animal: animal)
+                //Facts
+                //Description
+                //Map
+                //Link
+            } //: Vstack
+        } //: Scroll
+        
+    }
+}
+
+//MARK: - PREVIEW
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalDetailView(animal: animals[0])
+    }
+}
